6e07c7af2d0d996f871b9d8308f65e01fdb72649
Amel Gouila <amel.ghouila@gmail.com>
2017-11-14 08:33:28 +0100

    Update mapping.sh

--- Scripts/mapping.sh
+++ Scripts/mapping.sh
@@ -6,13 +6,17 @@
 #bowtie2-build -f genome.fa genomename #if you are using Bowtie as mapper
 #generate the bwa index if you are using bwa as mapper
 #The command below creates files for alignment. 
-bwa index reference.fasta
-#bwa index -a bwtsw reference.fa
-# -a bwtsw specifies that we want to use the indexing algorithm that is capable of handling the whole human genome
+bwa index -p genomename reference.fasta 
+#bwa index  -p genomename -a bwtsw reference.fa
+# you might use the option -a to specify the indexing algorithm. This option depends on the length of your genome
+# -a bwtsw specifies that we want to use the indexing algorithm that is capable of handling the whole human genome and does not work for short genomes
+# -a is does not work for long genomes
 
 #once your index is ready, you can launch the mapping
 
-bwa mem genome.fasta read1.fastq read2.fastq > alignedreads.sam
+bwa mem reference.fasta read1.fastq read2.fastq > alignedreads.sam # for PE sequencing 
+#make sure you give a significative name to your resulting sam file
+
 
 
 
