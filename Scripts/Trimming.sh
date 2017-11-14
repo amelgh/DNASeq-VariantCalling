3926061875b3f24000dec5918dae9fb7bfcbe5fd
Amel Gouila <amel.ghouila@gmail.com>
2017-11-14 05:01:08 +0100

    Update Trimming.sh

--- Scripts/Trimming.sh
+++ Scripts/Trimming.sh
@@ -9,7 +9,7 @@
 
 cd ~/untrimmed_fastq #your path to the original fastq files (non trimmed) 
 
-java -jar /opt/Trimmomatic-0.35/trimmomatic-0.35.jar PE \ #PE for Paired-end, if you have single End reads, use #SE
+java -jar /hpc/opt/conda/envs/trimmomatic@0.36/share/trimmomatic-0.36-5/trimmomatic.jar PE \ #PE for Paired-end, if you have single End reads, use #SE
 	-threads 4 \
 	-phred33 \ # TOPHRED33: Convert quality scores to Phred-33
 	. <input1>\
