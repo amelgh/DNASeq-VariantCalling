106b81d304e54f346625c83498efc7d361146860
Amel Gouila <amel.ghouila@gmail.com>
2017-11-14 04:02:13 +0100

    Update QC.sh

--- Scripts/QC.sh
+++ Scripts/QC.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+#PBS -l nodes=1:ppn=4 #you may use up to 40 CPU cores (depending on your job)
+#PBS -l mem=20gb
 
 module load fastqc
-fastqc fastqfile
+fastqc fastqfile  #only one sample
+#you can fastqc many fastq files on the same time
+#fastqc fastqfile1 fastqfile2 .... #You can specify as many files as you like
 
